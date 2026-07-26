08e66a687f82fc97fc47712523e2cc5005dd0132
yulikafsd <ju.zagorovsky@gmail.com>
2026-07-26 17:50:28 +0200

    add styles +  components

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import "modern-normalize";
 import "./global.css";
-import App from './App.tsx'
+import App from './components/App/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

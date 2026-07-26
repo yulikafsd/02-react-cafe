15b1c229fd2c7739108d2907b848251624c5360a
yulikafsd <ju.zagorovsky@gmail.com>
2026-07-26 19:43:15 +0200

    add render of components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,56 @@
-// import { useState } from 'react';
-// import css from './App.module.css';
-// import clsx from "clsx";
+import { useState } from 'react';
+import css from './App.module.css';
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/VoteStats';
+import Notification from '../Notification/Notification';
+import type { Votes, VoteType } from '../../types/votes';
+
+const initialVotes: Votes = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+};
 
 function App() {
-  return <></>;
+    const [votes, setVotes] = useState<Votes>(initialVotes);
+
+    function handleVote(type: VoteType): void {
+        setVotes((prevVotes) => ({
+            ...prevVotes,
+            [type]: prevVotes[type] + 1,
+        }));
+    }
+
+    function resetVotes(): void {
+        setVotes(initialVotes);
+    }
+
+    const { good, neutral, bad } = votes;
+    const totalVotes = good + neutral + bad;
+    const positiveRate = totalVotes
+        ? Math.round((votes.good / totalVotes) * 100)
+        : 0;
+
+    return (
+        <div className={css.app}>
+            <CafeInfo />
+            <VoteOptions
+                onVote={handleVote}
+                onReset={resetVotes}
+                canReset={totalVotes > 0}
+            />
+            {totalVotes > 0 ? (
+                <VoteStats
+                    votes={votes}
+                    totalVotes={totalVotes}
+                    positiveRate={positiveRate}
+                />
+            ) : (
+                <Notification />
+            )}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Notification/Notification.tsx
@@ -0,0 +1,9 @@
+import css from './Notification.module.css';
+
+function Notification() {
+  return (
+    <p className={css.message}>No feedback yet</p>
+  );
+}
+
+export default Notification;
